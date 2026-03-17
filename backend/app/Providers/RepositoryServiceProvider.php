fd73c47dd93fbd0dc47f495bb64e79806ce174a1
eguikuma <eguikuma@gmail.com>
2026-03-18 00:00:00 +0900

    feat: バックエンドの骨子を作成する

---
+++ backend/app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * リポジトリのインターフェースと実装をバインドする
+ */
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * サービスの登録
+     */
+    public function register(): void {}
+}
