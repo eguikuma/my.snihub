9ac8cfec3d29d932a0645941ba3206876702a332
eguikuma <eguikuma@gmail.com>
2026-03-23 00:00:00 +0900

    fix: ローカルのみでシーダーが実行されるようにする

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(LocalSeeder::class);
+        if (app()->environment('local')) {
+            $this->call(LocalSeeder::class);
+        }
     }
 }
